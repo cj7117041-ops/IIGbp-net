fa2456f5b52ad057ed1de80347786340da4f95e1
cj7117041-ops <cj7117041@gmail.com>
2026-03-21 20:24:03 +0800

    MyTest.py

--- IGBP-Net-github/IGBP-Net-github/Trans1/MyTest.py
+++ IGBP-Net-github/IGBP-Net-github/Trans1/MyTest.py
@@ -1,26 +1,18 @@
-# coding=utf-8
 import cv2
 import numpy as np
 import os
 
+cla_path = r'..root\data\ETIS-LaribPolypDB\masks' + '/'  
+masks = os.listdir(cla_path)  # iamges 
 
-
-cla_path = r'E:\yanghe\TransFuse\TransFuse-main\data\ETIS-LaribPolypDB\masks' + '/'  
-#E:\yanghe\TransFuse\TransFuse-main\dataset\dataset_offic\dataset\TrainDataset\images
-#E:\yanghe\TransFuse\TransFuse-main\data\ETIS-LaribPolypDB
-masks = os.listdir(cla_path)  
-#
 num = len(masks)
 for index, mask in enumerate(masks):
     img = cv2.imread(cla_path+mask, 0)
-    # img = cv2.GaussianBlur(img, (3, 3), 0)
     canny = cv2.Canny(img, 50, 150)  
     kernel = np.ones((4, 4), np.uint8)
     canny = (cv2.dilate(canny, kernel, iterations=1))
 
-    cv2.imwrite(r'E:\yanghe\TransFuse\TransFuse-main\data\ETIS-LaribPolypDB\boundary_dilation' + '/' + mask, canny)
-    # E:\yanghe\TransFuse\TransFuse-main\dataset\kvasir_clinicDB\Kvasir\boundary_dilation
-    # image_path.split('/')[-1]
+    cv2.imwrite(r'..root\data\ETIS-LaribPolypDB\boundary_dilation' + '/' + mask, canny)
 
 
 
